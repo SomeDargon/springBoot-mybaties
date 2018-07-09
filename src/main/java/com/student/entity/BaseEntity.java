b1645083db4e1003c91d98ce958f96b9a519d498
xiaolong <296569845@qq.com>
2018-07-09 17:14:10 +0800

    update html

---
+++ src/main/java/com/student/entity/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.student.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+@Data
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+}
---
+++ src/main/java/com/student/entity/Menu.java
@@ -0,0 +1,13 @@
+package com.student.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table
+public class Menu {
+}
