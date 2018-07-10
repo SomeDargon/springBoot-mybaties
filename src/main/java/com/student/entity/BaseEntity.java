d77625194a23ccc0bdc4480f4f084cd561da842e
xiaolong <296569845@qq.com>
2018-07-10 15:38:04 +0800

    update file

--- src/main/java/com/student/entity/BaseEntity.java
+++ src/main/java/com/student/entity/BaseEntity.java
@@ -2,12 +2,59 @@ package com.student.entity;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.Date;
 
 
 @Data
 public class BaseEntity implements Serializable {
 
+    private static final long serialVersionUID = 4796912106778982509L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    /** 搜索值 */
+    private String searchValue;
+    /** 创建者 */
+    private String createBy;
+    /** 创建时间 */
+    private Date createTime;
+    /** 更新者 */
+    private String updateBy;
+    /** 更新时间 */
+    private Date updateTime;
+    /** 备注 */
+    private String remark;
+
+    @Override
+    public int hashCode() {
+        return id == null ? System.identityHashCode(this) : id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass().getPackage() != obj.getClass().getPackage()) {
+            return false;
+        }
+        final BaseEntity other = (BaseEntity) obj;
+        if (id == null) {
+            if (other.getId() != null) {
+                return false;
+            }
+        } else if (!id.equals(other.getId())) {
+            return false;
+        }
+        return true;
+    }
 
 }
--- src/main/java/com/student/entity/Menu.java
+++ src/main/java/com/student/entity/Menu.java
@@ -3,11 +3,50 @@ package com.student.entity;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 @Table
-public class Menu {
+public class Menu extends BaseEntity {
+
+    @Column(name = "menu_name")
+    private String menuName;
+
+
+    /** 父菜单名称 */
+    @Column(name = "parent_name")
+    private String parentName;
+
+    /** 父菜单ID */
+    @Column(name = "parent_id")
+    private Long parentId;
+
+    /** 显示顺序 */
+    @Column(name = "order_num")
+    private String orderNum;
+
+    /** 菜单URL */
+    private String url;
+
+    /** 类型:0目录,1菜单,2按钮 */
+    @Column(name = "menu_type")
+    private String menuType;
+
+    /** 菜单状态:0显示,1隐藏 */
+    private int visible;
+
+    /** 权限字符串 */
+    private String perms;
+
+    /** 菜单图标 */
+    private String icon;
+
+    /** 子菜单 */
+    private List<Menu> children = new ArrayList<Menu>();
+
 }
